065ddedb0704d26e28b1b3a139c6cd2651b3d62a
Luke Archer <lukearcherdesigner@gmail.com>
2026-05-18 01:20:09 +0100

    Feat: Added useNavigate hook to make global Toast notification clickable link to Cart

--- client/src/components/Toast.jsx
+++ client/src/components/Toast.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Toast = ({ product }) => {
+  const navigate = useNavigate();
+
   if (!product) return null;
+
   return (
-    <div className="toast-container">
+    <div 
+      className="toast-container" 
+      onClick={() => navigate("/cart")}
+      style={{ cursor: "pointer" }}
+    >
       <img 
         src={`http://localhost:5001${product.image[0]}`} 
         alt={product.name} 
